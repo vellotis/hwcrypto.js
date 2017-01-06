f1c227850a6034742bb2f778d5a47bdb6727b55c
Kaarel Raspel <kaarel.raspel@gmail.com>
2017-01-06 09:34:53 +0200

    Add DigiDocServlet backend implementation

--- src/interfaces/index.js
+++ src/interfaces/index.js
@@ -1,7 +1,9 @@
 import DigiDocPlugin from './DigiDocPlugin'
 import DigiDocExtension from './DigiDocExtension'
+import DigiDocServlet from './DigiDocServlet'
 
 const enabledBackends = [
+    DigiDocServlet,
     DigiDocPlugin,
     DigiDocExtension
 ]
